7a4cf492f1958586a03b86981a10ef360450f505
Namrata <ztlab145@Zignutss-Mac-mini-5.local>
2024-10-01 19:06:40 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// document.getElementById("menu-toggle").addEventListener("click", function() {
+//     document.getElementById("wrapper").classList.toggle("toggled");
+//   });
+
+//   // JavaScript for attendance chart (placeholder)
+//   const attendanceChart = document.getElementById('attendance-chart');
+//   attendanceChart.innerHTML = "<img src='chart-placeholder.png' alt='Attendance Chart' />";
+
+// JavaScript to handle the toggle functionality
+document
+  .getElementById("sidebar-toggle")
+  .addEventListener("click", function () {
+    document.getElementById("sidebar-wrapper").classList.toggle("collapsed");
+  });
+
+  document
+  .getElementById("title")
+  .addEventListener("click", function () {
+    document.getElementById("sidebar-wrapper").classList.toggle("collapsed");
+  });
